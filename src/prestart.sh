b8c26f3af22ae01e580679c7f672e18cbbae53e3
deymonster <deymonster@yandex.ru>
2024-05-30 10:55:35 +0500

    Add setting permissions in prestart.sh

--- src/prestart.sh
+++ src/prestart.sh
@@ -2,6 +2,15 @@
 
 echo "Running prestart script..."
 
+echo "Setting permissions to media directory..."
+chown -R ${UID}:${GID} /app/media
+
+find /app/media -type d -exec chmod 775 {} ;
+find /app/media -type f -exec chmod 664 {} ;
+
+echo "Updating database..."
 
 alembic upgrade head
+
+echo "Starting application..."
 
