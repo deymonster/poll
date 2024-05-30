c01403a59dd9db66eb561e103695502afac81471
deymonster <deymonster@yandex.ru>
2024-05-30 10:59:41 +0500

    Fix syntax error in command in prestart.sh

--- src/prestart.sh
+++ src/prestart.sh
@@ -5,8 +5,8 @@ echo "Running prestart script..."
 echo "Setting permissions to media directory..."
 chown -R ${UID}:${GID} /app/media
 
-find /app/media -type d -exec chmod 775 {} ;
-find /app/media -type f -exec chmod 664 {} ;
+find /app/media -type d -exec chmod 775 {} \;
+find /app/media -type f -exec chmod 664 {} \;
 
 echo "Updating database..."
 
